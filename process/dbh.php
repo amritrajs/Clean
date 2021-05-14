d2393f6370232181543c6107c5c6910a96de929d
amritrajs <83804879+amritrajs@users.noreply.github.com>
2021-05-14 11:39:56 +0530

    Update dbh.php

--- process/dbh.php
+++ process/dbh.php
@@ -1,10 +1,14 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dbPassword = "";
-$dBName = "ems";
-
+//$servername = "localhost";
+//$dBUsername = "root";
+//$dbPassword = "";
+//$dBName = "ems";
+//for remote
+$servername = "remotemysql.com";
+$dBUsername = "WqHtgwVRVp";
+$dbPassword = "9iU4M8K3qQ";
+$dBName = "WqHtgwVRVp";
 $conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
 
 if(!$conn){
